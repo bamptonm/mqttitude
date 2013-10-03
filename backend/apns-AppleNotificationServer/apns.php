83e54ec916041915aa5b488d5bbf3dbe56ec2507
Christoph Krey <krey.christoph@gmail.com>
2013-10-03 09:59:43 +0200

    Revert "Merge branch 'master' of https://github.com/binarybucks/mqttitude"
    This reverts commit 1cc73d59dce63d8608624c3c0979e2e76757896f, reversing
    changes made to 99181af1e20a709b3fee613011f17e2fd3f71da9.

--- backend/apns-AppleNotificationServer/apns.php
+++ backend/apns-AppleNotificationServer/apns.php
@@ -1,18 +1,10 @@
 <?php
-<<<<<<< HEAD
 $deviceToken="bd41949a c6f94c34 ab190cb1 93e246be 859cc5d8 488c916f 8d696656 8f1725d6";
-=======
-$deviceToken="9d9ed2f7 60780c69 1a4727e6 d428cbe7 7fd84bad 9ab16206 5afd8f4c f12feab3";
->>>>>>> 459c8592bf1d3ec71f4b9d7d6040b5803ef217f5
 $payload['aps'] = array('alert' => 'Your Dad asks you to update your location');
 $output = json_encode($payload);
 $apnsHost = 'gateway.sandbox.push.apple.com';
 $apnsPort = 2195;
-<<<<<<< HEAD
 $apnsCert = 'apns-dev.pem';
-=======
-$apnsCert = 'apns-dev-cert-bundle.pem';
->>>>>>> 459c8592bf1d3ec71f4b9d7d6040b5803ef217f5
 $streamContext = stream_context_create();
 stream_context_set_option($streamContext, 'ssl', 'local_cert', $apnsCert);
 $apns = stream_socket_client('ssl://' . $apnsHost . ':' . $apnsPort, $error, $errorString, 2, STREAM_CLIENT_CONNECT, $streamContext);
@@ -21,7 +13,3 @@ $apnsMessage = chr(0) . chr(0) . chr(32) . pack('H*', str_replace(' ', '', $devi
 fwrite($apns, $apnsMessage);
 fclose($apns);
 ?>
-<<<<<<< HEAD
-=======
-
->>>>>>> 459c8592bf1d3ec71f4b9d7d6040b5803ef217f5
